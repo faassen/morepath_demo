4dc16e768f7d53e8c2ad5ba144097d5596d34eca
faassen <faassen@startifact.com>
2013-10-31 16:06:19 +0100

    This can now be nicer thanks to caller_package borrowed from Pyramid.

--- mpdemo/app.py
+++ mpdemo/app.py
@@ -49,8 +49,7 @@ def main():
     morepath.setup()
     # load application specific configuration
     config = morepath.Config()
-    import mpdemo
-    config.scan(mpdemo)
+    config.scan()
     config.commit()
 
     # serve app as WSGI app
